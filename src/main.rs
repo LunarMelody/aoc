3fe859211521033b30f35c65aab07a18fb246b4c
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-12-04 02:08:58 +0400

    added clap :D

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 mod day2;
 
+use clap::Parser;
 use day2::solution;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   #[arg(short, long, default_value_t = 1)]
+   day: u8,
+}
+
 fn main() {
-    solution::part2();
+    let args = Args::parse();
+
+    match args.day {
+        2 => {
+            solution::part1();
+            solution::part2();
+        },
+        _ => panic!("I didn't solve this :(")
+    }
 }
