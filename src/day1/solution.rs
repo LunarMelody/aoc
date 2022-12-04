af061b604fa4c030d4daaa1e366bf5fc09a88e9f
TenkoSpirit <103903444+TenkoSpirit@users.noreply.github.com>
2022-12-04 12:46:46 +0400

    added day 1 part 2

--- src/day1/solution.rs
+++ src/day1/solution.rs
@@ -1,8 +1,8 @@
 use super::input::INPUT_DATA;
 
 pub fn part1() {
-    let elfs = INPUT_DATA.split("\n\n");
-    let most_calories = elfs
+    let elves = INPUT_DATA.split("\n\n");
+    let most_calories = elves
         .map(|inv| {
             inv.lines()
                 .map(|item| item.parse::<i32>().unwrap())
@@ -11,9 +11,27 @@ pub fn part1() {
         .max()
         .unwrap();
 
-    println!("Elf carrying {} calories has the most calories!", most_calories);
+    println!(
+        "Elf carrying {} calories has the most calories!",
+        most_calories
+    );
 }
 
 pub fn part2() {
+    let backpacks = INPUT_DATA.split("\n\n");
 
+    let mut calories_per_elf: Vec<u32> = backpacks
+        .map(|backpack| {
+            backpack
+                .lines()
+                .map(|entry| entry.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect();
+
+    calories_per_elf.sort_by(|a, b| b.cmp(a));
+
+    let top_three: u32 = calories_per_elf.iter().take(3).sum();
+
+    println!("Top three: {:?}", top_three);
 }
